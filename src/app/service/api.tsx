e4b66e0f1f915c74e95a2c527c5bcaf8e7d3d5b2
Sophia Phu <sophiatiffphu@gmail.com>
2025-03-31 18:49:59 -0500

    pls workkkkk

--- src/app/service/api.tsx
+++ src/app/service/api.tsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-
 const api = axios.create({
     baseURL: "https://bubbleflowbackend.onrender.com/",
-    headers : {
+    headers: {
         'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*'
     },
+    withCredentials: false
 });
-
 export default api;
